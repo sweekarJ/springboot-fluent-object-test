dc9dc741366fba391ab2a71e803cd1c6fbfe5216
sweekarJ <joshi.sweekar@gmail.com>
2025-02-04 19:47:13 +0000

    Update PfxBasedHttpClient.java

--- src/test/java/com/example/digital/automation/stepdefinitions/PfxBasedHttpClient.java
+++ src/test/java/com/example/digital/automation/stepdefinitions/PfxBasedHttpClient.java
@@ -5,72 +5,100 @@ import org.apache.commons.httpclient.params.HttpMethodParams;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.net.ssl.SSLContext;
-import java.io.InputStream;
-import java.security.KeyStore;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
-import java.security.cert.X509Certificate;
+import java.io.InputStream;
+import java.security.KeyStore;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.httpclient.protocol.Protocol;
 
 public class PfxHttpClient {
 
     public static void main(String[] args) throws Exception {
-        String certPath = "certs/your-cert.pfx";
-        String certPassword = "your-cert-passphrase";
-        String url = "https://your-secure-api.com";
+        // ... (certificate loading code - same as before)
 
-        ClassPathResource resource = new ClassPathResource(certPath);
-        try (InputStream certStream = resource.getInputStream()) {
-            if (certStream == null) {
-                throw new RuntimeException("Certificate file not found: " + certPath);
+        // Create a trust manager that does not validate certificate chains (INSECURE - REPLACE IN PRODUCTION)
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
             }
-
-            KeyStore keyStore = KeyStore.getInstance("PKCS12");
-            keyStore.load(certStream, certPassword.toCharArray());
-
-            // Create a trust manager that does not validate certificate chains
-            TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-                public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                }
-                public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                }
-            } };
-
-            SSLContext sc = SSLContext.getInstance("TLS");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-
-            HttpClient client = new HttpClient();
-            client.getHostConfiguration().setHost("your-secure-api.com", 443, new org.apache.commons.httpclient.protocol.Protocol("https", new org.apache.commons.httpclient.contrib.HttpSslProtocolSocketFactory(sc), 443));
-
-            PostMethod method = new PostMethod(url);
-            method.addRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-            Map<String, String> body = new HashMap<>();
-            body.put("grant_type", "client_credentials");
-            body.put("client_id", "your-client-id");
-            body.put("client_secret", "your-client-secret");
-
-            NameValuePair[] data = body.entrySet().stream()
-                    .map(entry -> new NameValuePair(entry.getKey(), entry.getValue()))
-                    .toArray(NameValuePair[]::new);
-
-            method.setRequestBody(data);
-
-            int statusCode = client.executeMethod(method);
-
-            if (statusCode != 200) {
-                System.err.println("HTTP Status Code: " + statusCode);
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
             }
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        } };
 
-            String responseBody = method.getResponseBodyAsString();
+        SSLContext sc = SSLContext.getInstance("TLS");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
 
-            System.out.println("Response: " + responseBody);
+        Protocol.registerProtocol("https", new Protocol("https", new MySecureProtocolSocketFactory(sc), 443)); // Use custom socket factory
 
-            method.releaseConnection();
+        HttpClient client = new HttpClient();
+
+        PostMethod method = new PostMethod(url);
+        method.addRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        Map<String, String> body = new HashMap<>();
+        body.put("grant_type", "client_credentials");
+        body.put("client_id", "your-client-id");
+        body.put("client_secret", "your-client-secret");
+
+        NameValuePair[] data = body.entrySet().stream()
+                .map(entry -> new NameValuePair(entry.getKey(), entry.getValue()))
+                .toArray(NameValuePair[]::new);
+
+        method.setRequestBody(data);
+
+        int statusCode = client.executeMethod(method);
+
+        if (statusCode != 200) {
+            System.err.println("HTTP Status Code: " + statusCode);
         }
+
+        String responseBody = method.getResponseBodyAsString();
+
+        System.out.println("Response: " + responseBody);
+
+        method.releaseConnection();
+    }
+}
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.IOException;
+import java.net.Socket;
+import org.apache.commons.httpclient.protocol.SecureProtocolSocketFactory;
+
+class MySecureProtocolSocketFactory implements SecureProtocolSocketFactory {
+
+    private SSLContext sslContext = null;
+
+    public MySecureProtocolSocketFactory(SSLContext sslContext) {
+        this.sslContext = sslContext;
+    }
+
+    public Socket createSocket(String host, int port) throws IOException {
+        SSLSocketFactory factory = sslContext.getSocketFactory();
+        return factory.createSocket(host, port);
+    }
+
+    public Socket createSocket(String host, int port, java.net.InetAddress localAddress, int localPort) throws IOException {
+        SSLSocketFactory factory = sslContext.getSocketFactory();
+        return factory.createSocket(host, port, localAddress, localPort);
+    }
+
+
+    public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException {
+        SSLSocketFactory factory = sslContext.getSocketFactory();
+        return factory.createSocket(socket, host, port, autoClose);
+    }
+
+    public boolean equals(Object obj) {
+        return (obj == this);
+    }
+
+    public int hashCode() {
+        return this.getClass().hashCode();
     }
 }
